516cd59b4a72b05c6430b74e320242bcda614a65
Eric Krawczyk <eric@docnetwork.org>
2016-04-03 23:23:02 -0400

    add instructions, solution, and example output

--- fight.solution.js
+++ fight.solution.js
@@ -11,22 +11,31 @@ class Character {
 }
 
 Character.prototype.attackCharacter = function(defender) {
-  // Implement me!
+  var damage = this.attack - defender.defense;
+  damage += Math.floor(Math.random() * 6);
+  defender.health -= damage;
+  console.log(this.name + " does " + damage + " damage to " + defender.name);
 }
 
 // Main Fight Logic
 var player = new Character('Edward Norton', 100, 25, 20);
 var enemy  = new Character('Tyler Durden', 100, 25, 20);
 var round  = 1;
-
+//
 while (player.health && enemy.health) {
   runRound(round, player, enemy);
   round++;
-  console.log('');
+  console.log();
 }
 
 function runRound(round, p1, p2) {
-  // Implement me!
+  console.log(`----- Round ${round} -----`);
+  p1.attackCharacter(p2);
+  if (p2.health <= 0) { return endGame(p1, p2); }
+  p2.attackCharacter(p1);
+  if (p1.health <= 0) { return endGame(p2, p1); }
+  console.log(p1.name + ' health: ' + p1.health);
+  console.log(p2.name + ' health: ' + p2.health);
 }
 
 function endGame(winner, loser) {
